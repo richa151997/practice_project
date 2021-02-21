0d3b7b95b9761754647f91b7d6f3f8a753b89dbd
richa151997 <richa151997@gmail.com>
2021-02-21 20:54:37 +0530

    first commit

--- src/index.js
+++ src/index.js
@@ -1,13 +1,95 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+// #1
+// function App() {
+//   return (
+//       <div>
+//           <Header />
+//           <Greeting />
+//       </div>
+//   )
+// }
+
+class App extends React.Component {
+  constructor() {
+    super()
+    this.state = {
+      lastName: "Agrawal",
+      isLoggedIn: false
+    }
+  }
+  render() {
+    return (
+      <div>
+        <h1>User is Logged {this.state.isLoggedIn ? "in" : "out"}</h1>
+        <Header username={`Richa ${this.state.lastName}`} />
+        <Greeting />
+      </div>
+    )
+  }
+}
+
+// #2
+// function Header(props) {
+//   return (
+//     <header>
+//       <p>Welcome, {props.username}!</p>
+//     </header>
+//   )
+// }
+
+class Header extends React.Component {
+  render(props) {
+    return (
+      <header>
+        <p>Welcome, {this.props.username}!</p>
+      </header>
+    )
+  }
+}
+
+// #3
+// function Greeting() {
+//   const date = new Date()
+//   const hours = date.getHours()
+//   let timeOfDay
+
+//   if (hours < 12) {
+//     timeOfDay = "morning"
+//   } else if (hours >= 12 && hours < 17) {
+//     timeOfDay = "afternoon"
+//   } else {
+//     timeOfDay = "night"
+//   }
+//   return (
+//     <h1>Good {timeOfDay} to you, sir or madam!</h1>
+//   )
+// }
+
+class Greeting extends React.Component {
+  render() {
+    const date = new Date()
+    const hours = date.getHours()
+    let timeOfDay
+
+    if (hours < 12) {
+      timeOfDay = "morning"
+    } else if (hours >= 12 && hours < 17) {
+      timeOfDay = "afternoon"
+    } else {
+      timeOfDay = "night"
+    }
+    return (
+      <h1>Good {timeOfDay} to you, sir or madam!</h1>
+    )
+  }
+}
+
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <App />,
   document.getElementById('root')
 );
 
